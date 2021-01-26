687ea2bb12deb2495aad358b174de66335d5a6d4
Vincent-Boateng <vincentboateng@gmail.com>
2021-01-26 15:26:21 -0500

    More work  on  movie1 branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
         System.out.println("21 future SDET learning Git");
 
         System.out.println("Watching  movies  doesn't  have to  create a conflict");
+
+        System.out.println("Ready to go  back to living room to watch a movie");
     }
 }
