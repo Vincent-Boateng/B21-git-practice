f349c60383b2759fd5a824c5fae31bdfcb3d7efc
Vincent-Boateng <vincentboateng@gmail.com>
2021-01-26 15:19:32 -0500

    initial commit to movie1  new branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, 21");
         //added  some awesome test
         System.out.println("21 future SDET learning Git");
+
+        System.out.println("Watching  movies  doesn't  have to  create a conflict");
     }
 }
