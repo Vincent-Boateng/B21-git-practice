a9dcdd985711bf6ac037f76323e21cb975f352c3
Vincent-Boateng <vincentboateng@gmail.com>
2021-01-26 13:43:46 -0500

    B21 learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         //added  some awesome test
+        System.out.println("21 future SDET learning Git");
     }
 }
