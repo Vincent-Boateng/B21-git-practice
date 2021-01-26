a1a4016c7dd893cb0f4aa410cb44f7973d406ca6
Vincent-Boateng <vincentboateng@gmail.com>
2021-01-26 16:29:13 -0500

    Movie3 doesn't seem to have any conflicts so far

--- src/test/java/SeeingDifference.java
+++ src/test/java/SeeingDifference.java
@@ -1,4 +1,9 @@
 public class SeeingDifference {
     //this is new line of work in movie3
     //this is a new line of work. We are making progress
+
+    public static void main(String[] args) {
+
+        System.out.println("I am learning about git conflicts");
+    }
 }
